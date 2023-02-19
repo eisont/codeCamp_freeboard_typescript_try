bb03fc54d37047c796e9bc45626bda41399f054f
eisont <rlaclgns321@naver.com>
2023-02-19 15:55:58 +0900

    _app.tsx 기본 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,22 @@
 import type { AppProps } from "next/app";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Layout from "../src/components/commons/layout";
+import ApolloSetting from "../src/components/commons/apollo";
+import { RecoilRoot } from "recoil";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const client = new ApolloClient({
-    uri: "https://backend09.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(),
-  });
-
   return (
-    <ApolloProvider client={client}>
-      <Layout>
-        <Global styles={globalStyles} />
-        <Component {...pageProps} />;
-      </Layout>
-    </ApolloProvider>
+    <RecoilRoot>
+      <ApolloSetting>
+        <Layout>
+          <Global styles={globalStyles} />
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloSetting>
+    </RecoilRoot>
   );
 };
 
