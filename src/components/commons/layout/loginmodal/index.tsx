71a3767949ace1343413f23ed740471ca20f27f7
eisont <rlaclgns321@naver.com>
2023-02-19 15:46:45 +0900

    login 모달 추가

---
+++ src/components/commons/layout/loginmodal/index.tsx
@@ -0,0 +1,75 @@
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { RecoilModal } from "../../../../commons/store";
+import { Closesvg } from "../../../../commons/styles/Iconsvg";
+import { CodeCampLogosvg } from "../../../../commons/styles/Imgsvg";
+
+const ModalBox = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-232px, -153px);
+  z-index: 8888;
+  width: 464px;
+  height: 306px;
+  background: #fff;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const CloseIcon = styled.div`
+  position: absolute;
+  top: 23.5px;
+  right: 23.5px;
+  cursor: pointer;
+`;
+const Text = styled.div`
+  margin: 40px 0;
+  color: #000;
+  font-weight: 400;
+  font-size: 22px;
+`;
+const Bt = styled.div`
+  width: 384px;
+  height: 64px;
+  border-radius: 16px;
+  background: #ffd600;
+  border-radius: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: 700;
+  font-size: 16px;
+  cursor: pointer;
+`;
+interface IProps {
+  Title: String;
+}
+const LoginModal = (pr: IProps) => {
+  const router = useRouter();
+
+  const [, setModalClose] = useRecoilState(RecoilModal);
+
+  const onClickClose = () => {
+    setModalClose(false);
+    router.push("/login");
+  };
+
+  return (
+    <ModalBox>
+      <CloseIcon onClick={onClickClose}>
+        <Closesvg width="22" height="22" fill="#000" />
+      </CloseIcon>
+      <CodeCampLogosvg width="236" height="36" fill="#000" />
+
+      <Text>{pr.Title !== undefined ? pr.Title : "로그인이 필요합니다."}</Text>
+      <Bt onClick={onClickClose}>확인</Bt>
+    </ModalBox>
+  );
+};
+
+export default LoginModal;
